12da724bed1dbadb679eae222c3f5d41607ee439
GustavoGPL <gustavoguillan23@gmail.com>
2025-01-02 14:04:41 -0300

    Adjusting resend api key

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactFormEmail from "@/email/contact-form-email";
 
-const resend = new Resend(process.env.resendAPIKey);
+const resend = new Resend('re_H2L2mSir_MBv5VCbDhTj5p4FAnTUDVUva');
 
 export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
